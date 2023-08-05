80a47dd47a439b7ff7b927870cf3f2257933c25c
Rob Zolkos <rob@zolkos.com>
2023-08-05 15:55:05 -0400

    Initial commit

---
+++ test/controllers/posts_controller_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+class PostsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @post = posts(:one)
+  end
+
+  test "should get index" do
+    get posts_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_post_url
+    assert_response :success
+  end
+
+  test "should create post" do
+    assert_difference("Post.count") do
+      post posts_url, params: { post: { body: @post.body, title: @post.title } }
+    end
+
+    assert_redirected_to post_url(Post.last)
+  end
+end
